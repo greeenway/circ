a0aa805976f5f1482f892d794ff2ce40a16fa580
Lukas Nagel <lukaswnagel@gmail.com>
2011-07-08 16:59:25 +0200

    added double buffered canavas

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ class DrawPanel(wx.Window):
         wx.Window.__init__(self, parent, style=wx.NO_FULL_REPAINT_ON_RESIZE)
         self.Bind(wx.EVT_PAINT, self.OnPaint)
         wx.EVT_SIZE(self, self.OnSize)
+        wx.EVT_MOTION(self, self.onMouseOver)
     
     def OnPaint(self, event=None):
         #draw buffer to the screen
@@ -26,20 +27,36 @@ class DrawPanel(wx.Window):
         self.Refresh()
         self.Update()
         
+    def onMouseOver(self, event):
+        #(x-x_m)^2+(y-y_m)^2 < r^2
+        print '(' + str(event.GetX()) + '/' + str(event.GetX()) + ')'
+        
     def Draw(self, dc):
         dc.Clear()
         dc.SetBrush(wx.Brush("white"))
-        dc.DrawRectangle(10,10,100,100)
+        #dc.DrawRectangle(10,10,100,100)
         
-        dc.SetPen(wx.Pen("green", width=2) )
+        dc.SetPen(wx.Pen("black", width=1) )
+    
         red = wx.Colour(255,0,0)
         brush = wx.Brush(red, wx.TRANSPARENT)
+        self.DrawGrid(dc, 300, 300)
         dc.SetBrush(brush)
         
-        dc.DrawLine(0, 10, 50, 50)
+        #dc.DrawLine(0, 10, 50, 50)
         
-        dc.DrawRectangle(10,10,50,50)
-        
+        #dc.DrawRectangle(10,10,50,50)
+    
+    def DrawGrid(self, dc, width, height, delta=15):
+        x = delta
+        y = delta
+        while x < width:
+            while y < height:
+                dc.DrawCircle(x, y, 2)
+                y = y + delta
+            y = delta
+            x = x + delta
+                
 
         
         
